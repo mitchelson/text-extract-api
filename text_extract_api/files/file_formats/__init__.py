5adc0058e8e49674d81b5a7361707a58d88a5221
Adrian Chojnicki <adrian@chojnicki.pl>
2025-04-26 16:02:00 +0200

    + add docling to init import

--- text_extract_api/files/file_formats/__init__.py
+++ text_extract_api/files/file_formats/__init__.py
@@ -4,5 +4,6 @@
 ###    Run ./scripts/dev/gen-file-format-init.sh from repository root.
 
 from .file_format import FileFormat
+from .docling import DoclingFileFormat
 from .pdf import PdfFileFormat
 from .image import ImageFileFormat
